3bc72c9475323175736af6a8f40b2c81b84a3388
maggies2000 <95663039+maggies2000@users.noreply.github.com>
2021-12-06 17:08:36 -0800

    Create ProjectNotebook.py

---
+++ ProjectNotebook.py
@@ -0,0 +1,8 @@
+from my_module.classes import ExtroversionQuiz
+!pytest my_module/test_functions.py
+
+run = ExtroversionQuiz()
+run.run_extroversion_quiz()
+run.score_analysis()
+run.extroversion_prediction()
+run.quiz_results()
